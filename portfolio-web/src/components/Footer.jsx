eeaa6fb1baeef1b650d9d78a67f1ea231921b0f2
cherylpinto <cp16november@gmail.com>
2025-02-11 14:50:39 +0530

    final

--- portfolio-web/src/components/Footer.jsx
+++ portfolio-web/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
   return (
     <footer className="footer z-10 border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-10  flex flex-col justify-center items-center gap-2 lg:flex-row lg:justify-between md:pl-40 ">
+      <div className="container p-10  flex flex-col justify-center items-center gap-2 lg:flex-row lg:justify-between">
         <img src={logo} alt="Logo" className=" h-10 w-40 lg:h-12 lg:w-60" />
         <p className="text-slate-500 mt-2">
           &copy; {currentYear}{" "}
