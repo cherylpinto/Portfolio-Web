ff50104c2d555b41a819d779999ab4b540c619fe
cherylpinto <cp16november@gmail.com>
2025-02-13 11:52:51 +0530

    final

--- portfolio-web/src/components/ProjectCard.jsx
+++ portfolio-web/src/components/ProjectCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { CodeBracketIcon,EyeIcon } from "@heroicons/react/24/solid";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/solid";
 import { Link } from "react-router-dom";
 
-const ProjectCard = ({ imgUrl, title, description,gitUrl,previewUrl }) => {
+const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   const handlePreviewClick = (e) => {
     if (!previewUrl) {
       e.preventDefault();
@@ -11,25 +11,32 @@ const ProjectCard = ({ imgUrl, title, description,gitUrl,previewUrl }) => {
   };
   return (
     <div className="flex flex-col h-full">
-      <div
-        className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      >
-        <div
-          className="overlay flex items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] opacity-0 group-hover:opacity-60 transition-all duration-500"
-        >
-            <Link to={gitUrl} className="h-14 w-14 mr-2 border-2 border-[#ADB7BE] rounded-full relative hover:border-white group/link">
-                <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white"/>
-            </Link>
-            <Link to={previewUrl} onClick={handlePreviewClick} className="h-14 w-14 border-2 border-[#ADB7BE] rounded-full relative hover:border-white group/link">
-                <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white"/>
-            </Link> 
+      <div className="h-52 lg:h-72 rounded-t-xl relative group">
+        <img
+          src={imgUrl}
+          alt={title}
+          className="w-full h-52 lg:h-64 object-contain lg:object-cover rounded-lg"
+        />
+        <div className="overlay flex items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] opacity-0 group-hover:opacity-60 transition-all duration-500">
+          <Link
+            to={gitUrl}
+            className="h-14 w-14 mr-2 border-2 border-[#ADB7BE] rounded-full relative hover:border-white group/link"
+          >
+            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+          </Link>
+          <Link
+            to={previewUrl}
+            onClick={handlePreviewClick}
+            className="h-14 w-14 border-2 border-[#ADB7BE] rounded-full relative hover:border-white group/link"
+          >
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
+          </Link>
         </div>
       </div>
 
-      <div className="text-white mt-2 rounded-b-xl bg-[#181818] py-6 px-4 flex-grow">
+      <div className="text-white sm-mt-2 rounded-b-xl bg-[#181818] py-3 sm:py-6 px-4 flex-grow">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
-        <p className="text-[#ADB7BE]"> {description} </p>
+        <p className="text-[#ADB7BE] text-xs sm:text-base "> {description} </p>
       </div>
     </div>
   );
